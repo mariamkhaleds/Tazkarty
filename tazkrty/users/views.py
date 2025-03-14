50e1b7385af6374f63943dd13fac36932bbd64f2
Mariamwageeh123 <mw487371@gmail.com>
2025-03-14 14:21:16 +0200

    Update views.py

--- tazkrty/users/views.py
+++ tazkrty/users/views.py
@@ -29,25 +29,18 @@ def user_registration(request):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
 
-from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from rest_framework.authtoken.models import Token
+from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.decorators import api_view
 from rest_framework import status
+from .serializers import UserLoginSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 
-@api_view(['POST'])
-def login_view(request):
-    username = request.data.get("username")
-    password = request.data.get("password")
-    
-    user = authenticate(username=username, password=password)
-    
-    if user:
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({"token": token.key}, status=status.HTTP_200_OK)
-    else:
-        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
+class UserLoginView(APIView):
+    def post(self, request):
+        serializer = UserLoginSerializer(data=request.data)
+        if serializer.is_valid():
+            return Response(serializer.validated_data, status=200)
+        return Response(serializer.errors, status=400)
 
 
 """""
